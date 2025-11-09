77387444ebd4b05bcde0bbbc76ceb6b9d66f5f72
Igor Kotenko <ikotenko2006@gmail.com>
2025-11-09 23:34:20 +0300

    saveTasks in js and give names to elements

--- code.js
+++ code.js
@@ -1,1 +1,10 @@
 const task_name = document.getElementById("name_task");
+const addButton = document.getElementById("add");
+const taskList = document.querySelector(".my_list");
+
+let currentFilter = "all";
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
