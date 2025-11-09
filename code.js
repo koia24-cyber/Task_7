a9e162a44961c2edc46c58289776bfbb35eb4b30
Igor Kotenko <ikotenko2006@gmail.com>
2025-11-09 23:37:52 +0300

    js: addButton.onclick and displayTasks

--- code.js
+++ code.js
@@ -8,3 +8,18 @@ let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+
+
+addButton.onclick = () => {
+    const text = task_name.value.trim();
+    if (text) {
+        tasks.push({ text, completed: false, timestamp: Date.now() });
+        saveTasks();
+        task_name.value = "";
+        displayTasks();
+    }
+};
+
+function displayTasks() {
+    
+}
